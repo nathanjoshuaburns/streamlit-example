74ae9acd83d036489aecae9a6f6f67ed386fb89c
Nathan Burns <nathan.burns@baringa.com>
2022-10-07 14:05:27 +0100

    empty df catering

--- weather_icon.py
+++ weather_icon.py
@@ -15,25 +15,29 @@ import pandas as pd
 #gettempandhumid(48.8567, 2.3522, datetime(2015,1,1,12,0,0))
 
 def get_icon(coords, selectedDate) -> str:
-    
+
     selectedDateAsDateTime = datetime.combine(selectedDate, datetime.min.time())
 
     point = Point(coords[0], coords[1])
     df = Hourly(point, selectedDateAsDateTime, selectedDateAsDateTime).fetch()
-    humidity = df.rhum[0]
-    temperature = df.temp[0]
 
-    icon_name = "cloud" # Default to cloud
-
-    if temperature >= 30 and humidity > 100:
-        icon_name = "sun-o"
-    elif temperature >= 50:
-        icon_name = "fire"
-    elif temperature >= 30:
-        icon_name = "sun-o"
-    elif humidity > 100:
-        icon_name = "tint"
+    if df.empty:
+        return "cloud"
     else:
-        icon_name = "cloud"
+        humidity = df.rhum[0]
+        temperature = df.temp[0]
 
-    return icon_name
+        icon_name = "cloud" # Default to cloud
+
+        if temperature >= 30 and humidity > 100:
+            icon_name = "sun-o"
+        elif temperature >= 50:
+            icon_name = "fire"
+        elif temperature >= 30:
+            icon_name = "sun-o"
+        elif humidity > 100:
+            icon_name = "tint"
+        else:
+            icon_name = "cloud"
+
+        return icon_name
