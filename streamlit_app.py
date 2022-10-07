14d43c7cd429739e12145ea7896a6c3b0f5134da
Nathan Burns <nathan.burns@baringa.com>
2022-10-07 11:05:52 +0100

    Added line chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,11 @@ timeOfDay = st.slider(
     value=time(12, 0),
     step=timedelta(minutes=60))
 
+st.line_chart(
+    data=None,
+    x="Hours",
+    y="Price")
+
 # center on Liberty Bell, add marker
 m = folium.Map(location=[46.6714327602744, 2.5419523299087947], zoom_start=6)
 folium.Marker(
