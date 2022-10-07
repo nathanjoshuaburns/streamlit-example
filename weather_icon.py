863853fb2bec5ea62adc56285f0f227bc38eb502
Tomas Chuaqui <Tomas.Chuaqui.extern@rwe.com>
2022-10-07 16:16:59 +0100

    Added Eesa's bit for weather on map

--- weather_icon.py
+++ weather_icon.py
@@ -1,5 +1,5 @@
 import random
-from meteostat import Point, Hourly
+from meteostat import Hourly, Stations
 from datetime import datetime, date
 import pandas as pd
 
@@ -11,33 +11,53 @@ import pandas as pd
 #    rhum = df.rhum[0]
 #    temp = df.temp[0]
 #    return {'humidity': rhum, 'temperature': temp}
-
 #gettempandhumid(48.8567, 2.3522, datetime(2015,1,1,12,0,0))
+def gettempandhumid(long, lat, day):
+    stations = Stations()
+    nearbystations = stations.nearby(long, lat).fetch(100)
+
+    for nstation in nearbystations.wmo:
+        try:
+
+            df = Hourly(nstation, day, day).fetch()
+
+            rhum = df.rhum[0]
+
+        except Exception:
+
+            continue
+        break
+    for nstation in nearbystations.wmo:
+        try:
+
+            df = Hourly(nstation, day, day).fetch()
+
+            temp = df.temp[0]
+
+        except Exception:
+
+            continue
+        break
+    return {'humidity': rhum, 'temperature': temp}
 
 def get_icon(coords, selectedDate) -> str:
 
     selectedDateAsDateTime = datetime.combine(selectedDate, datetime.min.time())
 
-    point = Point(coords[0], coords[1])
-    df = Hourly(point, selectedDateAsDateTime, selectedDateAsDateTime).fetch()
+    tempdict = gettempandhumid(coords[0], coords[1], selectedDateAsDateTime)
 
-    if df.empty:
-        return "cloud"
+    humidity = tempdict['humidity']
+    temperature = tempdict['temperature']
+
+    icon_name = "cloud" # Default to cloud
+    if temperature >= 30 and humidity > 100:
+        icon_name = "sun-o"
+    elif temperature >= 50:
+        icon_name = "fire"
+    elif temperature >= 30:
+        icon_name = "sun-o"
+    elif humidity > 100:
+        icon_name = "tint"
     else:
-        humidity = df.rhum[0]
-        temperature = df.temp[0]
-
-        icon_name = "cloud" # Default to cloud
-
-        if temperature >= 30 and humidity > 100:
-            icon_name = "sun-o"
-        elif temperature >= 50:
-            icon_name = "fire"
-        elif temperature >= 30:
-            icon_name = "sun-o"
-        elif humidity > 100:
-            icon_name = "tint"
-        else:
-            icon_name = "cloud"
-
-        return icon_name
+        icon_name = "cloud"
+    return icon_name
