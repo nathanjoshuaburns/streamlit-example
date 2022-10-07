a438396ae425c0b55a1498131171980039325a79
Nathan Burns <nathan.burns@baringa.com>
2022-10-07 13:53:20 +0100

    proper call to weather

--- weather_icon.py
+++ weather_icon.py
@@ -5,25 +5,23 @@ import pandas as pd
 
 
 
-def gettempandhumid(long, lat, day):
-    point = Point(long, lat)
-    df = Hourly(point, day, day).fetch()
-    rhum = df.rhum[0]
-    temp = df.temp[0]
-    return {'humidity': rhum, 'temperature': temp}
+#def gettempandhumid(long, lat, day):
+#    point = Point(long, lat)
+#    df = Hourly(point, day, day).fetch()
+#    rhum = df.rhum[0]
+#    temp = df.temp[0]
+#    return {'humidity': rhum, 'temperature': temp}
 
-
-
-gettempandhumid(48.8567, 2.3522, datetime(2015,1,1,12,0,0))
+#gettempandhumid(48.8567, 2.3522, datetime(2015,1,1,12,0,0))
 
 def get_icon(coords, selectedDate) -> str:
-    icon_name = "cloud" # Default to cloud
-    temperature = 0
-    humidity = 0
 
-    # Generate test data
-    temperature = random.randint(16, 60)
-    humidity = random.randint(50, 150)
+    point = Point(coords[0], coords[1])
+    df = Hourly(point, selectedDate, selectedDate).fetch()
+    humidity = df.rhum[0]
+    temperature = df.temp[0]
+
+    icon_name = "cloud" # Default to cloud
 
     if temperature >= 30 and humidity > 100:
         icon_name = "sun-o"
