1059765946474b803307b3b359171455f752284d
Nathan Burns <nathan.burns@baringa.com>
2022-10-07 11:40:16 +0100

    date

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ timeOfDay = st.slider(
 
 selectedDate = st.date_input(
     "Date",
-    datetime.date(2016, 7, 6))
+    datetime(2016, 7, 6))
 
 hour = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23']
 samplePrices = [
